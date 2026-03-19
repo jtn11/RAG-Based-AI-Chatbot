2ecf26107955e35f258890145bc870e64f66ecc5
jtn11 <jatinsainii007@gmail.com>
2026-03-19 13:30:38 +0530

    cors fix -deploy prep

--- RAG-pipelines/main.py
+++ RAG-pipelines/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app
+    allow_origins=["*"],  # Allow all domains including local and Vercel deployments
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
